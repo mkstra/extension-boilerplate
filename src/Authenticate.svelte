9afaee243e6580782b27041a6c994b61df4d77a0
Markus <markus@markuss-air.localdomain>
2019-11-09 18:01:46 +0100

    worked the whole day to end up where I started .. thank you CHROME and WEB extension API

--- src/Authenticate.svelte
+++ src/Authenticate.svelte
@@ -1,8 +1,6 @@
 <script>
-/*global chrome*/
+import LoginForm from './LoginForm.svelte'
 
-import { authState } from 'rxfire/auth';
-  // Your web app's Firebase configuration
 const chromestoreID = "bedmbgkddhhnajfafgpbpbcjmdnnngll"
 //...bf is localhost
 const localDevID = "ldhfcelpkklpbfafamncdmgenaphmibf"
@@ -10,15 +8,7 @@ const EXTENSION_ID = localDevID
   //TODO: Attention - chrome id on localhost isn't same as chrome store
 //To send messages to content scripts, use tabs.sendMessage.
 
-  let user;
-
-
-window.c = chrome.runtime.sendMessage
-chrome.runtime.sendMessage(EXTENSION_ID, {greeting: "svelte"}, async function(response) {
-  console.log("response: bg -> auth", response.farewell);
-  return true
-});
-
+//FIXME: firebase is injected from the index.html script
 const firebaseConfig = {
     apiKey: "AIzaSyDC1R-8N_K9ExqoUqlY_hv3Hsq-95fL7XU",
     authDomain: "together-7d90d.firebaseapp.com",
@@ -29,70 +19,20 @@ const firebaseConfig = {
     appId: "1:1051151523557:web:43e5075d31f4722731be0c",
     measurementId: "G-279CSGB8R1"
   };
+// Initialize Firebas
 
-// Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth()
-  console.log("below")
-  const unsubscribe = authState(auth).subscribe(u => {
-    console.log("user here", u); 
-    //TODO: is there a better way than mutating user?
-    user = u
-   
+//FIXME: waiting for firebase to load into it....
+const app = firebase.initializeApp(firebaseConfig);
 
-
-  }
-  );
-  const isExistingUser = (currentUser) => (currentUser.metadata.lastSignInTime - currentUser.metadata.creationTime) > 1000
-	const ui = new firebaseui.auth.AuthUI(auth);
-	const uiConfig = {
-          credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-          callbacks: {
-          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            // Do something with the returned AuthResult.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            console.log("authResult", authResult)
-            return false;
-          },
-          signInFailure: function(error) {
-            // Return a promise when error handling is completed and FirebaseUI
-            // will reset, clearing any UI. This commonly occurs for error code
-            // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
-            // occurs. Check below for more details on this.
-            console.error("---sign in FAILURE")
-            return false
-          },
-          uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
-            document.getElementById('loader').style.display = 'none';
-          }},
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-	  }
-  ],
-  signInSuccessUrl: "/",
-}
-
-  const loginUI = (force) => {
-  if (force) {
-    ui.start('#firebaseui-auth-container', uiConfig);
-  }
-}
-loginUI(ui.isPendingRedirect())
-
-
+console.log("app fiba", app)
 </script>
-    <div id="firebaseui-auth-container"></div>
-     <div id="loader">Loading...</div>
-{#if user}
-    <button on:click={() => auth.signOut() }>Logout</button>
-    <hr>
-{:else}
-   <button on:click={loginUI}>
-		Signin 
-	</button>
+
+<h1>App here</h1>
+
+{#if app}
+  <LoginForm app={app} />
 {/if}
+
+
+
  
---
+++ src/LoginForm.svelte
@@ -0,0 +1,68 @@
+<script>
+import { authState } from 'rxfire/auth';
+
+export let app
+console.log("APPP", app)
+//TODO: very hacky because of chrome hostility... 
+const {firebase_ : firebase} = app
+
+const auth = firebase.auth()
+const isExistingUser = (currentUser) => (currentUser.metadata.lastSignInTime - currentUser.metadata.creationTime) > 1000
+const ui = new firebaseui.auth.AuthUI(auth);
+const uiConfig = {
+        credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
+        callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+          // Do something with the returned AuthResult.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          console.log("authResult", authResult)
+          return false;
+        },
+        signInFailure: function(error) {
+          // Return a promise when error handling is completed and FirebaseUI
+          // will reset, clearing any UI. This commonly occurs for error code
+          // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+          // occurs. Check below for more details on this.
+          console.error("---sign in FAILURE")
+          return false
+        },
+        uiShown: function() {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }},
+        signInOptions: [{
+          provider: firebase["auth"].EmailAuthProvider.PROVIDER_ID,
+        
+        }],
+        signInSuccessUrl: "/",
+      }
+
+const loginUI = (force) => {
+  if (force) {
+    ui.start('#firebaseui-auth-container', uiConfig);
+    }
+}
+
+loginUI(ui.isPendingRedirect())
+
+
+let user;
+const unsubscribe = authState(auth).subscribe(u => {
+    console.log("user here", u); 
+    //TODO: is there a better way than mutating user?
+    user = u 
+});
+
+</script>
+    <div id="firebaseui-auth-container"></div>
+     <div id="loader">Loading...</div>
+{#if user}
+    <button on:click={() => auth.signOut() }>Logout</button>
+    <hr>
+{:else}
+   <button on:click={loginUI}>
+		Signin 
+	</button>
+{/if}
