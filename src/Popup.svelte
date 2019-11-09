46f32a360c427ebf16ae7ab7a1611dab954ed4ad
Markus <markus@markuss-air.localdomain>
2019-11-09 12:44:45 +0100

    listener inside popup

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -4,5 +4,22 @@
     
     let uid
     //TODO: listen to message {User}
+
+    chrome.runtime.onMessage
+    .addListener(function(request, sender, sendResponse) {
+            console.log(
+                sender.tab 
+                ? "from a content script:" + sender.tab.url 
+                : "from the extension")
+
+    if (request.uid) {
+        uid = "fat mongo"
+        sendResponse({res: "got it"});
+        
+    }
+    else console.log("AAAH")
+
+
+  });
 </script>
 <Profile {uid} />
